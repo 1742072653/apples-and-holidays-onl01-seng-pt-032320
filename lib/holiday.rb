d016a3498f99ed88a77894958b613f738907479f
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 20:58:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
       arr= []
       i=0
       string << "  " 
-      arr = key.to_s.split("_") ["new" ,"year"]
+      arr = key.to_s.split("_") 
       while i < arr.size 
         arr[i]=  arr[i].capitalize
         i += 1
