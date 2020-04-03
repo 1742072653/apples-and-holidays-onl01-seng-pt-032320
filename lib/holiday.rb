7ed6a5939597767a499a64eb262eb35143690242
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 20:14:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key,value|
-    puts string = " key + ": "
+    puts string =  key + ": "
     value.each do |key,value|
       puts key + ": " + value.join(",")
     end
