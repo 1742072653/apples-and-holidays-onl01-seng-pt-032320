6eab5205de9689aa428e076f75aacc1a281f52c3
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 20:14:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key,value|
     puts key.to_s + ": "
     value.each do |key,value|
-      puts key + ": " + value.join(",")
+      puts key.to_s + ": " + value.join(",")
     end
   end
 end
