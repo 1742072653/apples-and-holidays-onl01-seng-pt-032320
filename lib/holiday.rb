95a584b7fc47b2c1f2d9d1cb6dea52707471fbe8
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 20:20:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,6 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  arr = []
   holiday_hash.each do |key,value|
     if key == :winter
        return value.values.flatten
