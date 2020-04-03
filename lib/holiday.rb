a0c1934c9e096b3e0db94bc711c73ebdff166ce6
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 20:09:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidays=[]
+  holiday_hash.each do |key.value|
+    value.each do |key,value|
+      if value.include?("BBQ")
+        holidays << key
+      end
+    end
+  end
 end
 
 
